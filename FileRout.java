debe8cdbee35d66a009076bd15d6dd4ee7d5b1ca
SowmyaKuntamukkala <61749487+SowmyaKuntamukkala@users.noreply.github.com>
2021-02-16 18:08:50 +0530

    Update FileRout.java

--- FileRout.java
+++ FileRout.java
@@ -1,16 +1,36 @@
-package com.example.demospringboot.routes;
+package com.camel.demospringcamel.routes;
 
+import com.example.demospringboot.processor.MyProcessor;
 import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class FileRoute extends RouteBuilder {
 
+    @Value("${app.source}")
+    private String sourcelocation;
+
+    @Value("${app.destination}")
+    private String destination;
+
+    @Autowired
+    private MyProcessor processor;
+
     @Override
     public void configure() throws Exception {
-        from("file:C:/Training/input?=fileName=Test_input.txt")
-                .to("file:C:/Training/output/")
-                .log(LoggingLevel.INFO, "File transferred successfully");
+
+        fileCopier();
+
+    }
+
+    private void fileCopier() {
+        from("file:" + sourcelocation).routeId("Demo-File-Route")
+//               .setBody(simple(" ${body}.I have updated the file content"))
+                .process(processor)
+                .to("file:" + destination)
+                .log(LoggingLevel.INFO, "File is moved to destination \n ${body}");
     }
 }
